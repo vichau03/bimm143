11cca3c3a3ea6283014800b51ad73e61ff509286
Vivian Chau <vchau@ucsd.edu>
2025-03-02 22:08:26 -0800

    Add Class 4 to directory

---
+++ Class 4/Class4.R
@@ -0,0 +1,12 @@
+#' ---
+#' title: "BIMM 143 - Class 4: Intro to R"
+#' author: "Vivian Chau (PID: A16913056)"
+#' date: "January 15th, 2025"
+#' ---
+
+x<-1:50
+plot(x,sin(x))
+
+# Make this nicer
+plot(x, sin(x), col="purple", type="l", lwd=3)
+
